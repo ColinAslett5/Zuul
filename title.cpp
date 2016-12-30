c8cb474b4564b6bed056adf05dfe9bf5f1c67c72
ColinAslett5 <280929@bsd48.org>
2016-12-30 10:26:09 -0800

    Second Commit

--- title.cpp
+++ title.cpp
@@ -4,13 +4,16 @@
 #include "room.h"
 #include <vector>
 #include <cstring>
+#include <map>
 //https://www.sgi.com/tech/stl/Map.html
+//http://www.cprogramming.com/tutorial/stl/stlmap.html
 using namespace std;
 void inv(vector<item*> &itema);
 //losing condition, you go into the room you lose in
 int main(){ 
   vector<item*> itema;
   vector<room*> rooma;
+  vector<item*> inventory;
   bool pop = true;
   //items
   item* flashlight = new item("it shines light on dark objects", false);
@@ -22,18 +25,19 @@ int main(){
   room* yourroom = new room("Your room");
   room* hallway = new room("the hallway");
   room* bathroom = new room("its a bathroom!");
-  room* playroom = new room("don't trip");
-  room* kitchen = new room("pots and pans that you cant pick up");
-  room* bonusroom = new room("there is a tv but no remote");
-  room* garage = new room("No cars are here :(");
-  room* basement = new room("don't go into the next room");
-  room* ifyougoinhereyouwilllose = new room("WHAT DID I TELL YOU! NOW YOU GOTTA PLAY THIS ENTIRE GAME AGAIN!");
-  room* attic = new room("dust and spiDERS!!!!");
+  room* playroom = new room("a playroom");
+  room* kitchen = new room("a room with pots and pans that you cant pick up");
+  room* bonusroom = new room("playroom, there is a tv but no remote");
+  room* garage = new room("No cars are in this garage :(");
+  room* basement = new room("Scary basement with a note: dont go into the next room!");
+  room* youwilllose = new room("WHAT DID I TELL YOU! NOW YOU GOTTA PLAY THIS ENTIRE GAME AGAIN!");
+  room* attic = new room("Attic: dust and spiDERS!!!!");
   room* parentsbedroom = new room("Why are you in your parents room?");
-  room* workroom = new room("Your father is looking at you, wondering what you are doing walking around the house");
+  room* workroom = new room("Your father is looking at you, wondering what you are doing walking around his workroom");
   room* mainhallway = new room("very big hallway, maybe there is a door to the outside here?");
   room* adoor = new room("its a door to the outside! But do you have the key?");
   room* familyroom = new room("a room with couches but no people");
+  room* currentroom = new room("no desc");
   //items in each room
   yourroom->itema.push_back(litpaper);
   playroom->itema.push_back(stick);
@@ -58,7 +62,7 @@ int main(){
   garage->exits["north"] = bonusroom;
   garage->exits["south"] = basement;
   basement->exits["north"] = garage;
-  basement->exits["south"] = ifyogoinhereyouwilllose;
+  basement->exits["south"] = youwilllose;
   //do i need one for the losing room? maybe?
   familyroom->exits["west"] = kitchen;
   familyroom->exits["south"] = workroom;
@@ -66,9 +70,25 @@ int main(){
   workroom->exits["north"] = familyroom;
   mainhallway->exits["west"] = familyroom;
   mainhallway->exits["south"] = adoor;
-  
+  //other variables
+  currentroom = kitchen;
   while(pop == true){
-    cout << "Welcome to Zuul, a game about a guy(or girl) that just wants to get outside";
+    char stra[6];
+    cout << "Welcome to Zuul, a game about a guy(or girl) that just wants to get outside" << endl;
+    cout << "You are currently in " << currentroom->getDesc() << endl;
+    cout << "There are exits:" << endl;
+    for(map<const char*, room*, ltstr>::iterator i = currentroom->exits.begin();i != currentroom->exits.end();i++){
+      cout << i->first << endl;
+    }
+    cin.get(stra,6);
+    for(map<const char*, room*, ltstr>::iterator i = currentroom->exits.begin();i != currentroom->exits.end();i++){
+      if(strcmp(stra,i->first) == 0){
+	currentroom = i->second;
+	break;
+      }
+    }
+    cout << currentroom->getDesc();
+    
     pop = false;
   }
 }
