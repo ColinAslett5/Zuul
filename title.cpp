b0eaaf56cba5d49e32d78b99165e95a48ec9f870
ColinAslett5 <280929@bsd48.org>
2016-12-30 20:19:45 -0800

    FINAL COMMIT

--- title.cpp
+++ title.cpp
@@ -27,7 +27,7 @@ int main(){
   room* bathroom = new room("its a bathroom!");
   room* playroom = new room("a playroom");
   room* kitchen = new room("a room with pots and pans that you cant pick up");
-  room* bonusroom = new room("playroom, there is a tv but no remote");
+  room* bonusroom = new room("bonusroom, there is a tv but no remote");
   room* garage = new room("No cars are in this garage :(");
   room* basement = new room("Scary basement with a note: dont go into the next room!");
   room* youwilllose = new room("WHAT DID I TELL YOU! NOW YOU GOTTA PLAY THIS ENTIRE GAME AGAIN!");
@@ -37,7 +37,7 @@ int main(){
   room* mainhallway = new room("very big hallway, maybe there is a door to the outside here?");
   room* adoor = new room("its a door to the outside! But do you have the key?");
   room* familyroom = new room("a room with couches but no people");
-  room* currentroom = new room("no desc");
+  room* currentroom;
   //items in each room
   yourroom->itema.push_back(litpaper);
   playroom->itema.push_back(stick);
@@ -48,10 +48,12 @@ int main(){
   yourroom->exits["west"] = hallway;
   hallway->exits["west"] = playroom;
   hallway->exits["north"] = bathroom;
-  hallway->exits["east"] = hallway;
+  hallway->exits["east"] = yourroom;
   hallway->exits["south"] = kitchen;
+  bathroom->exits["south"] = hallway;
   playroom->exits["north"] = attic;
   playroom->exits["south"] = parentsbedroom;
+  playroom->exits["east"] = hallway;
   attic->exits["south"] = playroom;
   parentsbedroom->exits["north"] = playroom;
   kitchen->exits["north"] = hallway;
@@ -70,8 +72,9 @@ int main(){
   workroom->exits["north"] = familyroom;
   mainhallway->exits["west"] = familyroom;
   mainhallway->exits["south"] = adoor;
+  adoor->exits["north"] = mainhallway;
   //other variables
-  currentroom = kitchen;
+  currentroom = yourroom;
   cout << "Welcome to Zuul, a game about a guy(or girl) that just wants to get outside" << endl;
   while(pop == true){
     char stra[6];//exits
@@ -95,14 +98,55 @@ int main(){
     }
     cout << "4 possible options('quit','move'.'pick','drop')" << endl;
     //picking/dropping item
-    cin.getline(strx,10);
+    cin.getline(strx,50);
     if(strcmp(strx,"quit") == 0){
       pop = false;
+      delete flashlight;
+      flashlight = NULL;
+      delete key;
+      key = NULL;
+      delete realkey;
+      realkey = NULL;
+      delete stick;
+      stick = NULL;
+      delete litpaper;
+      litpaper = NULL;
+      delete yourroom;
+      yourroom = NULL;
+      delete hallway;
+      hallway = NULL;
+      delete bathroom;
+      bathroom = NULL;
+      delete playroom;
+      playroom = NULL;
+      delete kitchen;
+      kitchen = NULL;
+      delete bonusroom;
+      bonusroom = NULL;
+      delete garage;
+      garage = NULL;
+      delete basement;
+      basement = NULL;
+      delete youwilllose;
+      youwilllose = NULL;
+      delete attic;
+      attic = NULL;
+      delete parentsbedroom;
+      parentsbedroom = NULL;
+      delete workroom;
+      workroom = NULL;
+      delete mainhallway;
+      mainhallway = NULL;
+      delete adoor;
+      adoor = NULL;
+      delete familyroom;
+      familyroom = NULL;
       return 0;
     }
+    //picking up the item
     if(strcmp(strx,"pick") == 0){
       char input[50];
-      cout << "enter items name that you want to pick up" << endl;
+      cout << "enter item's name that you want to pick up" << endl;
       cin.getline(input,50);
       bool temp = false;
       for(vector<item*>::iterator i = currentroom->itema.begin();i != currentroom->itema.end();i++){
@@ -118,12 +162,29 @@ int main(){
 	cout << "YOU DIDN'T PICK IT UP!" << endl;
       }
     }
+    //dropping the item
     if(strcmp(strx,"drop") == 0){
-
+      char inputx[50];
+      cout << "enter items that you want to drop" << endl;
+      cin.getline(inputx,50);
+      bool tempx = false;
+      for(vector<item*>::iterator i = inventory.begin();i != inventory.end();i++){
+	if(strcmp(inputx,((*i)->getDescription())) == 0){
+	  currentroom->itema.push_back(*i);
+	  inventory.erase(i);
+	  cout << "YOU DROPPED IT!" << endl;
+	  tempx = true;
+	  break;
+	}
+      }
+      if(tempx == false){
+	cout << "YOU DIDN'T DROP IT" << endl;
+      }
     }
     //to leave the room
     if(strcmp(strx,"move") == 0){
-      cin.getline(stra,6);
+      cout << "Enter a direction" << endl;
+      cin.getline(stra,50);
       for(map<const char*, room*, ltstr>::iterator i = currentroom->exits.begin();i != currentroom->exits.end();i++){
 	if(strcmp(stra,i->first) == 0){
 	  currentroom = i->second;
@@ -136,14 +197,67 @@ int main(){
       }
     }
     //pop = false;
+    //winning condition
     if(currentroom == adoor){
-      cout << "YOU WON!";
+      for(vector<item*>::iterator i = inventory.begin();i != inventory.end();i++){
+	if(strcmp("workingkey",((*i)->getDescription())) == 0){
+	  cout << "YOU WON!";
+          delete flashlight;
+          delete key;
+          delete realkey;
+          delete stick;
+          delete litpaper;
+          delete yourroom;
+          delete hallway;
+          delete bathroom;
+          delete playroom;
+          delete kitchen;
+          delete bonusroom;
+          delete garage;
+          delete basement;
+          delete youwilllose;
+          delete attic;
+          delete parentsbedroom;
+          delete workroom;
+          delete mainhallway;
+          delete adoor;
+          delete familyroom;
+	  pop = false;
+	  return 0;
+	}
+      }  
+    }
+    //losing condition
+    if(currentroom == youwilllose){
+      bool tempo = false;
+      if(tempo == false){
+	cout << "YOU SHOULD HAVE LISTENED TO THE WARNING!";
+	tempo = true;
+      }
+      delete flashlight;
+      delete key;
+      delete realkey;
+      delete stick;
+      delete litpaper;
+      delete yourroom;
+      delete hallway;
+      delete bathroom;
+      delete playroom;
+      delete kitchen;
+      delete bonusroom;
+      delete garage;
+      delete basement;
+      delete youwilllose;
+      delete attic;
+      delete parentsbedroom;
+      delete workroom;
+      delete mainhallway;
+      delete adoor;
+      delete familyroom;
       pop = false;
       return 0;
     }
     //pop = false;
   }
 }
-void inv(vector<item*> &xd){
-  
-}
+
